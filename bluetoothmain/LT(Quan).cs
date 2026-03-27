b5b4184695ba486cd8055733f824145ba43755ee
Thành Huỳnh <hhthanh.toan01k19.chuyenlongan@gmail.com>
2026-03-27 13:56:41 +0700

    fff

---
+++ bluetoothmain/LT(Quan).cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bluetoothmain
+{
+    public partial class LT : UserControl
+    {
+        public LT()
+        {
+            InitializeComponent();
+        }
+    }
+}
